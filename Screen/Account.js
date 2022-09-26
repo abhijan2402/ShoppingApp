681cda0c74981854ee5cf5754e51270f517f6a2e
abhijan2402 <abhishek.jangid643@gmail.com>
2022-09-26 19:59:09 +0530

    Adding seller profile page

--- Screen/Account.js
+++ Screen/Account.js
@@ -1,61 +1,122 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native'
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 function Account() {
+    const [seller, setSeller] = useState(false)
     return (
-        <View style={styles.AccountScreen}>
-            <View>
-                <TouchableOpacity style={styles.TOBackImg}>
-                    <Image
-                        style={styles.BackImg}
-                        source={{ uri: "https://cdn-icons-png.flaticon.com/512/130/130882.png" }}
-                    />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.UserProfileImgView}>
-                <Image
-                    style={styles.UserProfileImg}
-                    source={{ uri: "https://media.thetab.com/blogs.dir/90/files/2020/04/wig.jpg" }}
-                />
+        <>
+            {
+                seller ?
+                    <View style={styles.AccountScreen}>
+                        <View>
+                            <TouchableOpacity style={styles.TOBackImg} onPress={() => { setSeller(false) }}>
+                                <Image
+                                    style={styles.BackImg}
+                                    source={{ uri: "https://cdn-icons-png.flaticon.com/512/130/130882.png" }}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.UserProfileImgView}>
+                            <Image
+                                style={styles.UserProfileImg}
+                                source={{ uri: "https://img.freepik.com/free-vector/cartoon-style-cafe-front-shop-view_134830-697.jpg?w=2000" }}
+                            />
 
-            </View>
-            <View>
-                <View style={styles.MyProfileText}>
-                    <Text style={styles.MyProfileTextInner}>MyProfile</Text>
-                </View>
-                <View style={styles.DetailSection}>
-                    <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Name:ABhi</Text></View>
-                    <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Age:17</Text></View>
-                </View>
-                <View style={styles.ContactView}>
-                    <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Contact : 7976114625</Text></View>
+                        </View>
+                        <View>
+                            <View style={styles.MyProfileText}>
+                                <Text style={styles.MyProfileTextInner}>MyProfile</Text>
+                            </View>
+                            <View style={styles.DetailSection}>
+                                <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Name:ABhi</Text></View>
+                                <View style={styles.innerDetailSection}><Text style={[styles.innerDetailSectionText, { marginLeft: -20 }]}>Account Details</Text></View>
+                            </View>
+                            <View style={styles.ContactView}>
+                                <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Contact : 7976114625</Text></View>
 
-                </View>
-                <View style={styles.BlueLine}></View>
-            </View>
-            <View>
-                <View style={styles.OrderDetailSectionBtn}>
-                    <Image source={{ uri: "https://thumbs.dreamstime.com/b/grocery-order-bag-food-goods-online-shop-hands-holding-vector-illustration-doodles-thin-line-art-sketch-style-concept-181767788.jpg" }} style={styles.OrderImg} />
-                    <Text style={styles.OrderDetailSectionText} >Orders</Text>
-                </View>
-                <View style={styles.OrderDetailSectionBtn}>
-                    <Image source={{ uri: "https://cdn5.vectorstock.com/i/1000x1000/95/59/order-checklist-icon-cartoon-delivery-list-vector-39259559.jpg" }} style={styles.OrderImg} />
-                    <Text style={styles.OrderDetailSectionText} >Recently Ordered Seller</Text>
-                </View>
-                <View style={styles.OrderDetailSectionBtn}>
-                    <Image source={{ uri: "https://cdn-icons-png.flaticon.com/128/1041/1041916.png" }} style={styles.OrderImg} />
-                    <Text style={styles.OrderDetailSectionText} >Orders</Text>
-                </View>
-                <View style={styles.OrderDetailSectionBtn}>
-                    <Image source={{ uri: "https://png.pngtree.com/element_our/20200702/ourlarge/pngtree-cartoon-creative-map-location-image_2286149.jpg" }} style={styles.OrderImg} />
-                    <Text style={styles.OrderDetailSectionText} >Address</Text>
-                </View>
-            </View>
-        </View>
+                            </View>
+                            <View style={styles.BlueLine}></View>
+                        </View>
+                        <View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://thumbs.dreamstime.com/b/grocery-order-bag-food-goods-online-shop-hands-holding-vector-illustration-doodles-thin-line-art-sketch-style-concept-181767788.jpg" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >OrderLists</Text>
+                            </View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://img.freepik.com/premium-vector/special-offer-background-vector-icon-illustration-logo-mascot-hand-drawn-concept-trandy-cartoon_519183-280.jpg?w=2000" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >Offer Running</Text>
+                            </View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://cdn-icons-png.flaticon.com/128/1041/1041916.png" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >Chat with customer</Text>
+                            </View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://png.pngtree.com/element_our/20200702/ourlarge/pngtree-cartoon-creative-map-location-image_2286149.jpg" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >Shop location</Text>
+                            </View>
+                        </View>
+                    </View> :
+                    <View style={styles.AccountScreen}>
+                        <View>
+                            <TouchableOpacity style={styles.TOBackImg} onPress={() => { setSeller(true) }}>
+                                <Image
+                                    style={styles.BackImg}
+                                    source={{ uri: "https://cdn-icons-png.flaticon.com/512/130/130882.png" }}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.UserProfileImgView}>
+                            <Image
+                                style={styles.UserProfileImg}
+                                source={{ uri: "https://media.thetab.com/blogs.dir/90/files/2020/04/wig.jpg" }}
+                            />
+
+                        </View>
+                        <View>
+                            <View style={styles.MyProfileText}>
+                                <Text style={styles.MyProfileTextInner}>MyProfile1</Text>
+                            </View>
+                            <View style={styles.DetailSection}>
+                                <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Name:ABhi</Text></View>
+                                <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Age:17</Text></View>
+                            </View>
+                            <View style={styles.ContactView}>
+                                <View style={styles.innerDetailSection}><Text style={styles.innerDetailSectionText}>Contact : 7976114625</Text></View>
+
+                            </View>
+                            <View style={styles.BlueLine}></View>
+                        </View>
+                        <View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://thumbs.dreamstime.com/b/grocery-order-bag-food-goods-online-shop-hands-holding-vector-illustration-doodles-thin-line-art-sketch-style-concept-181767788.jpg" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >Orders</Text>
+                            </View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://cdn5.vectorstock.com/i/1000x1000/95/59/order-checklist-icon-cartoon-delivery-list-vector-39259559.jpg" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >Recently Ordered Seller</Text>
+                            </View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://cdn-icons-png.flaticon.com/128/1041/1041916.png" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >Orders</Text>
+                            </View>
+                            <View style={styles.OrderDetailSectionBtn}>
+                                <Image source={{ uri: "https://png.pngtree.com/element_our/20200702/ourlarge/pngtree-cartoon-creative-map-location-image_2286149.jpg" }} style={styles.OrderImg} />
+                                <Text style={styles.OrderDetailSectionText} >Address</Text>
+                            </View>
+                        </View>
+                    </View>
+
+            }
+        </>
+
+
+        // 
     )
 }
 const styles = StyleSheet.create({
+
+    // For Shopper
     AccountScreen: {
         backgroundColor: "white",
         height: windowHeight
@@ -63,7 +124,8 @@ const styles = StyleSheet.create({
     TOBackImg: {
         // borderWidth: 1,
         height: windowHeight / 15,
-        justifyContent: "center"
+        justifyContent: "center",
+        marginHorizontal: 10
     },
     BackImg: {
         width: 25,
@@ -114,8 +176,8 @@ const styles = StyleSheet.create({
         marginHorizontal: 30
     },
     OrderImg: {
-        width: 38,
-        height: 38
+        width: 35,
+        height: 35
 
     },
     OrderDetailSectionBtn: {
@@ -131,6 +193,11 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         // color: "black"
     }
+
+
+
+    // for Shopkepper
+
 
 });
 export default Account
