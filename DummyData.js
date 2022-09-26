218f272b1f828c55393e0a9e2e798431fd726ad8
Chirag <takchirag828@gmail.com>
2022-09-26 17:06:52 +0530

    home page fro seller

---
+++ DummyData.js
@@ -0,0 +1,27 @@
+const data=[
+    {
+        id:1,name:"Iphone 13",imageLink:"https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg",
+        mrp:"80,000",orders:10,offers:"No Offer",availability:"Available"
+    },
+    {
+        id:2,name:"Iphone 14",imageLink:"https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg",
+        mrp:"100,000",orders:5,offers:"No Offer",availability:"Unavailable"
+    },
+    {
+        id:3,name:"Iphone 14 Pro",imageLink:"https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg",
+        mrp:"120,000",orders:3,offers:"No Offer",availability:"Unavailable"
+    },
+    {
+        id:4,name:"Iphone 11",imageLink:"https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg",
+        mrp:"60,000",orders:6,offers:"20% Offer",availability:"Available"
+    },
+    {
+        id:5,name:"Iphone X",imageLink:"https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg",
+        mrp:"50,000",orders:0,offers:"10% Offer",availability:"Available"
+    },
+    {
+        id:6,name:"Iphone XR",imageLink:"https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg",
+        mrp:"40,000",orders:1,offers:"30% Offer",availability:"Available"
+    },
+]
+export default data;
--- Screen/Home.js
+++ Screen/Home.js
@@ -1,11 +1,130 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-function Home() {
+import { View, Text, StyleSheet,FlatList,Dimensions,Image, TouchableOpacity } from 'react-native'
+import data from '../DummyData'
+const windowWidth=Dimensions.get('window').width;
+const windowHeight=Dimensions.get('window').height;
+const logo1=require("../Assets/logo3.png");
+const search=require("../Assets/search.png");
+const Home=()=>{
+    const renderItem = ({ item }) => (
+        <View style={styles.itemCard}>
+            <Image
+                style={styles.itemImage}
+                source={{uri:item.imageLink}}
+            />
+            <View style={{paddingHorizontal:10,width:"70%"}}>
+                <View style={{flexDirection:"row",alignItems:'center',justifyContent:"space-between"}}>
+                    <Text style={[styles.text,{fontSize:20}]}>{item.name}</Text>
+                    <Text style={[styles.text,{fontSize:15}]}>Orders:{item.orders}</Text>
+                </View>
+                <View style={{flexDirection:"row",alignItems:'center',justifyContent:"space-between"}}>
+                    <Text style={[styles.text,{fontSize:15}]}>{item.mrp} Rs</Text>
+                    <Text style={[styles.text,{fontSize:15}]}>{item.offers} Applied</Text>
+                </View>
+                <View style={{flexDirection:"row",alignItems:'center',justifyContent:"space-between",width:"100%"}}>
+                    <TouchableOpacity style={styles.btnContainer}>
+                        <Text style={[styles.text,{color:'white'}]}>{item.availability}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnContainer}>
+                        <Text style={[styles.text,{color:'white'}]}>Edit</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={[styles.btnContainer,{width:'100%',alignSelf:"center"}]}>
+                    <Text style={[styles.text,{color:'white'}]}>All Orders</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
     return (
-        <View>
-            <Text>hi</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={{flexDirection:"row",alignItems:"center"}}>
+                    <Image 
+                        style={styles.iconImage}
+                        source={logo1}
+                    />
+                    <Text style={styles.showName}>Shop Name</Text>
+                </View>
+                <Image
+                    style={styles.iconImage}
+                    source={search}
+                />
+            </View>
+            <Text style={styles.itemText}>Items</Text>
+            <FlatList
+                data={data}
+                keyExtractor={item=>item.id}
+                renderItem={renderItem}
+                showsVerticalScrollIndicator={false}
+            />
         </View>
     )
 }
-
+const styles=StyleSheet.create({
+    header:{
+        width:windowWidth,
+        height:60,
+        backgroundColor:"white",
+        borderBottomLeftRadius:10,
+        borderBottomRightRadius:10,
+        justifyContent:"space-between",
+        padding:10,
+        flexDirection:'row',
+    },      
+    showName:{
+        fontWeight:"bold",
+        color:"#3063A0",
+        fontSize:20,
+        marginHorizontal:10
+    },
+    container:{
+        flex:1,
+        alignItems:"center",
+        // justifyContent:"center",
+        backgroundColor:"#3063A0"
+    },
+    itemCard:{
+        backgroundColor:"white",
+        width:windowWidth-30,
+        padding:10,
+        borderRadius:10,
+        // height:windowHeight/3.5,
+        flexDirection:"row",
+        marginTop:20
+    },
+    itemImage:{
+        height:120,
+        width:'30%',
+        resizeMode:'contain',
+        borderRadius:10
+    },
+    text:{
+        fontWeight:"bold",
+        color:"black"
+    },
+    itemText:{
+        fontWeight:"bold",
+        color:"white",
+        fontSize:30,
+        alignSelf:"flex-start",
+        padding:20
+    },
+    btnContainer:{
+        width:'50%',
+        height:30,
+        backgroundColor:"#3063A0",
+        alignItems:"center",
+        justifyContent:"center",
+        marginVertical:4,
+        marginHorizontal:1,
+        borderRadius:1000
+    },
+    iconImage:{
+        height:30,
+        width:30,
+        resizeMode:'contain',
+        // borderRadius:20
+    }
+})
 export default Home
+
