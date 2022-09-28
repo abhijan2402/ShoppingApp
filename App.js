03b2c6fee1ed1808aee38df0ceca6b7af1155ab0
aayutha <thatheraaayushi@gmail.com>
2022-09-28 09:05:47 +0530

    bottomtab

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Screen/Home';
@@ -11,11 +12,131 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer >
-      <Tab.Navigator  >
-        <Tab.Screen name="Home" component={Home} options={{ headerShown: false, }} />
-        <Tab.Screen name="Account" component={Account} options={{ headerShown: false, }} />
-        <Tab.Screen name="Chat" component={ChatRoom} options={{ headerShown: false, }} />
-        <Tab.Screen name="OrderPage" component={OrderPage} options={{ headerShown: false, }} />
+      <Tab.Navigator
+       tabBarOptions={{
+        showLabel: false,
+      }}
+      screenOptions={{
+        tabBarStyle: {
+          position: 'absolute',
+          backgroundColor: '#3063A0',
+          borderRadius: 15,
+          elevation: 15,
+          bottom: 10,
+          left: 20,
+          right: 20,
+          height: 70,
+        },
+      }}>
+        <Tab.Screen name="Home" component={Home} options={{ headerShown: false,
+         tabBarIcon: ({focused}) => (
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              top: 4,
+            }}>
+            <Image
+              source={require('../ShoppingApp/Assets/home.png')}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#ffffff' : '#748c94',
+              }}
+            />
+            <Text
+              style={{
+                color: focused ? '#ffffff' : '#748c94',
+                fontSize: 12,
+              }}>
+              Home
+            </Text>
+          </View>
+        ),
+        }} />
+        <Tab.Screen name="Account" component={Account} options={{ headerShown: false, 
+       tabBarIcon: ({focused}) => (
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            top: 4,
+          }}>
+          <Image
+            source={require('../ShoppingApp/Assets/account.png')}
+            resizeMode="contain"
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: focused ? '#ffffff' : '#748c94',
+            }}
+          />
+          <Text
+            style={{
+              color: focused ? '#ffffff' : '#748c94',
+              fontSize: 12,
+            }}>
+            User
+          </Text>
+        </View>
+      ),  
+      }} 
+        />
+        <Tab.Screen name="Chat" component={ChatRoom} options={{ headerShown: false, 
+         tabBarIcon: ({focused}) => (
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              top: 4,
+            }}>
+            <Image
+              source={require('../ShoppingApp/Assets/chat.png')}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#ffffff' : '#748c94',
+              }}
+            />
+            <Text
+              style={{
+                color: focused ? '#ffffff' : '#748c94',
+                fontSize: 12,
+              }}>
+              Chat
+            </Text>
+          </View>
+        ),
+        }} />
+        <Tab.Screen name="OrderPage" component={OrderPage} options={{ headerShown: false, 
+         tabBarIcon: ({focused}) => (
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              top: 4,
+            }}>
+            <Image
+              source={require('../ShoppingApp/Assets/orderpage.png')}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#ffffff' : '#748c94',
+              }}
+            />
+            <Text
+              style={{
+                color: focused ? '#ffffff' : '#748c94',
+                fontSize: 12,
+              }}>
+              Orders
+            </Text>
+          </View>
+        ),
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
